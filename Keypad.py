51cdb8d1f5238c9ef69fb893e92af41b4a5d8944
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-09 15:56:32 +0200

    cp - rp

--- Keypad.py
+++ Keypad.py
@@ -6,8 +6,8 @@ import RPi.GPIO as GPIO
 
 class Keypad:
     """ keypad class """
-    r_pins = [18, 23, 24, 25]
-    c_pins = [17, 27, 22]  # pins 18, 23, 24 and 25 for key rows 0, 1, 2 and 3
+    c_pins = [18, 23, 24, 25]
+    r_pins = [17, 27, 22]  # pins 18, 23, 24 and 25 for key rows 0, 1, 2 and 3
                                          # pins 17, 27 and 22 for key columns 0, 1 and 2
     keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]
 
@@ -15,18 +15,18 @@ class Keypad:
     def __init__(self):
         """ Set the mode and the row pins as outputs and the column pins as inputs """
         GPIO.setmode(GPIO.BCM)
-        for rp in range(0, 4):
+        for rp in range(0, 3):
             GPIO.setup(self.r_pins[rp], GPIO.OUT)
-        for cp in range(0, 3):
+        for cp in range(0, 4):
             GPIO.setup(self.c_pins[cp], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def do_polling(self):
         """ Use nested loops (discussed above) to determine the
         key currently being pressed on the keypad. """
         #print("KAYPAD DO POLLING")
-        for rp in range(0, 4):
+        for rp in range(0, 3):
             GPIO.output(self.r_pins[rp], GPIO.HIGH)
-            for cp in range(0, 3):
+            for cp in range(0, 4):
                 if GPIO.input(self.c_pins[cp]) == GPIO.HIGH:
                     print("FOUND HIGHS - SLEEP")
                     #time.sleep(10)
