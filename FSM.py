d113cbe850e76e895b7abf4d6d45c5167d5c5ce5
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-10 12:22:22 +0200

    hack

--- FSM.py
+++ FSM.py
@@ -97,7 +97,12 @@ class FiniteStateMachine:
             print("CURRENT STATE: ", self.state)
             self.signal = self.get_next_signal()
             if self.signal:
-                print("SIGNAL: ", self.signal)
-                self.run_rules()
+                self.KPC_pointer.set_led_id(self.signal)
+                self.KPC_pointer.add_to_buffer('3')
+                self.KPC_pointer.input_buffer_to_led_duration()
+
+                self.KPC_pointer.light_one_led()
+                #print("SIGNAL: ", self.signal)
+                #self.run_rules()
             print("\n")
         print("Final state reached.")
