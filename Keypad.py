ff85b7a4a89d49bd9400c529bc89cd70e1a4934f
Ola Hermann Opheim <olahop@stud.ntnu.no>
2019-10-10 10:10:27 +0200

    1

--- Keypad.py
+++ Keypad.py
@@ -6,8 +6,8 @@ import RPi.GPIO as GPIO
 
 class Keypad:
     """ keypad class """
-    c_pins = [18, 23, 24, 25]
-    r_pins = [17, 27, 22]  # pins 18, 23, 24 and 25 for key rows 0, 1, 2 and 3
+    r_pins = [18, 23, 24, 25]
+    c_pins = [17, 27, 22]  # pins 18, 23, 24 and 25 for key rows 0, 1, 2 and 3
                                          # pins 17, 27 and 22 for key columns 0, 1 and 2
     keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']]
 
@@ -15,24 +15,28 @@ class Keypad:
     def __init__(self):
         """ Set the mode and the row pins as outputs and the column pins as inputs """
         GPIO.setmode(GPIO.BCM)
-        for rp in range(0, 3):
+        for rp in range(0, 4):
+            print(self.r_pins[rp])
             GPIO.setup(self.r_pins[rp], GPIO.OUT)
-        for cp in range(0, 4):
+        for cp in range(0, 3):
+            print(self.c_pins[cp])
             GPIO.setup(self.c_pins[cp], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def do_polling(self):
         """ Use nested loops (discussed above) to determine the
         key currently being pressed on the keypad. """
         #print("KAYPAD DO POLLING")
-        for rp in range(0, 3):
+        for rp in range(0, 4):
             GPIO.output(self.r_pins[rp], GPIO.HIGH)
-            for cp in range(0, 4):
+            for cp in range(0, 3):
                 if GPIO.input(self.c_pins[cp]) == GPIO.HIGH:
                     print("FOUND HIGHS - SLEEP")
                     #time.sleep(10)
                     #print("DONT SLEEP")
                     if GPIO.input(self.c_pins[cp]) == GPIO.HIGH:
-                        print("SHOULD RETURN")
+                        #print("SHOULD RETURN")
+                        print("rp: " + rp)
+                        print("cp: " + cp)
                         print(self.keypad[rp][cp])
                         return self.keypad[rp][cp]
             GPIO.output(self.r_pins[rp], GPIO.LOW)
